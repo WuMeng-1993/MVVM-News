771bcba952946a08c93244f71bb58f43f2c6e883
WuMeng <541345568@qq.com>
2020-12-02 23:52:05 +0800

    1. 编写登录界面 2. 启用dataBinding

--- app/src/main/java/com/wumeng/mvvm_news/ui/LoginActivity.java
+++ app/src/main/java/com/wumeng/mvvm_news/ui/LoginActivity.java
@@ -1,15 +1,20 @@
-package com.wumeng.mvvm_news;
+package com.wumeng.mvvm_news.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.wumeng.mvvm_news.R;
+
+/**
+ * 登录界面
+ */
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
 }
