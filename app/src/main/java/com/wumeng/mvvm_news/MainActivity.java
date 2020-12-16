7f94acbfbe2d8fd36aa1581c5f17cec5917acf46
WuMeng <541345568@qq.com>
2020-12-17 00:14:00 +0800

    登录编写完成，开始编写MainActivity

--- app/src/main/java/com/wumeng/mvvm_news/MainActivity.java
+++ app/src/main/java/com/wumeng/mvvm_news/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author WuMeng
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/wumeng/mvvm_news/ui/login/LoginActivity.java
+++ app/src/main/java/com/wumeng/mvvm_news/ui/login/LoginActivity.java
@@ -5,16 +5,21 @@ import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.wumeng.mvvm_news.MainActivity;
 import com.wumeng.mvvm_news.R;
 import com.wumeng.mvvm_news.bean.BaseReqData;
 import com.wumeng.mvvm_news.bean.login.UserBean;
+import com.wumeng.mvvm_news.config.HttpConfig;
 import com.wumeng.mvvm_news.databinding.ActivityLoginBinding;
 
 /**
  * 登录界面
+ *
  * @author WuMeng
  */
 public class LoginActivity extends AppCompatActivity {
@@ -26,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mActivityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login);
+        mActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         mLoginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
         clickEvent();
     }
@@ -39,21 +44,38 @@ public class LoginActivity extends AppCompatActivity {
         mActivityLoginBinding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mLoginViewModel.login(getUserName(),getPass());
+                mLoginViewModel.login(getUserName(), getPass());
             }
         });
 
         mLoginViewModel.loginLiveData.observe(this, new Observer<BaseReqData<UserBean>>() {
             @Override
             public void onChanged(BaseReqData<UserBean> userBeanBaseReqData) {
-
+                if (userBeanBaseReqData.getStatue().equals(HttpConfig.REQUEST_OK)) {
+                    showToast(userBeanBaseReqData.getMsg());
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    showToast(userBeanBaseReqData.getMsg());
+                }
             }
         });
-        
+
+    }
+
+    /**
+     * Toast提示
+     *
+     * @param message
+     */
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     /**
      * 获取输入框的用户名
+     *
      * @return
      */
     private String getUserName() {
@@ -62,6 +84,7 @@ public class LoginActivity extends AppCompatActivity {
 
     /**
      * 获取输入框的密码
+     *
      * @return
      */
     private String getPass() {
