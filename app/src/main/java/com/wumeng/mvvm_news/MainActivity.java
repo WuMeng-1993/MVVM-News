1a9d43a3f03c863053ee8dd5d77a2af2038b3b47
WuMeng <541345568@qq.com>
2020-12-21 23:53:34 +0800

    底部导航栏与Fragment的切换

--- app/src/main/java/com/wumeng/mvvm_news/MainActivity.java
+++ app/src/main/java/com/wumeng/mvvm_news/MainActivity.java
@@ -1,18 +1,54 @@
 package com.wumeng.mvvm_news;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * @author WuMeng
  */
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView navView;
+
+    private FloatingActionButton btnChangeTheme;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navView = findViewById(R.id.bottom_nav_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard
+        ).build();
+
+        navView.setItemIconTintList(null);
+        navView.setItemRippleColor(null);
+
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+        NavigationUI.setupWithNavController(navView,navController);
+
+        navView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_SELECTED);
+
+        btnChangeTheme = findViewById(R.id.btn_change_theme);
+        btnChangeTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
 }
