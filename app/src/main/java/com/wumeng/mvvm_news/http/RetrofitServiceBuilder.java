fc9a29731c23ce28d71776e11c9c2a3ce7fd2b53
WuMeng <541345568@qq.com>
2020-12-28 23:52:17 +0800

    添加Retrofit相关的库以及创建Retrofit代码

---
+++ app/src/main/java/com/wumeng/mvvm_news/http/RetrofitServiceBuilder.java
@@ -0,0 +1,30 @@
+package com.wumeng.mvvm_news.http;
+
+import com.wumeng.mvvm_news.config.HttpConfig;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * @author WuMeng
+ * @date 2020/12/28
+ * desc:
+ */
+public class RetrofitServiceBuilder {
+
+    public static <T> T createService(final Class<T> clazz) {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(HttpConfig.BASE_API)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        T service = (T) retrofit.create(clazz);
+
+        return service;
+
+    }
+
+}
